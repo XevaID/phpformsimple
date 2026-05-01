9c6a4b9e674353041f13f73e70d23781e6302e07
Fandhi <fputraandhika@gmail.com>
2026-05-01 22:39:51 +0700

    simple

--- redquiff_app/conn.php
+++
@@ -1,9 +0,0 @@
-<?php
-$host = "127.0.0.1";
-$host_user = "root";
-$host_pw = "";
-$host_data_name = "data-be"; // Make sure to define your database name
-$kont = mysqli_connect($host, $host_user, $host_pw, $host_data_name);
-if (!$kont) {
-    die("Koneksi gagal: " . mysqli_connect_error());
-}
